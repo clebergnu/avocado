007523f0a621ce1d34488dca6e7f4872fa12dc51
Cleber Rosa <crosa@redhat.com>
2017-02-08 19:51:09 -0500

    selftests/unit/test_varianter.py: add unittests related to string methods
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ selftests/unit/test_varianter.py
@@ -0,0 +1,41 @@
+import unittest
+import argparse
+
+from avocado.core import varianter
+from avocado.core import mux
+
+
+class TestVarianter(unittest.TestCase):
+
+    def setUp(self):
+        self.tree = mux.MuxTreeNode('')
+        path = mux.MuxTreeNode('path', parent=self.tree)
+        path.multiplex = True
+        path1 = mux.MuxTreeNode('path1', {'key1': 'value1'}, parent=path)
+        path2 = mux.MuxTreeNode('path2', {'key2': 'value2'}, parent=path)
+        self.varianter = varianter.Varianter()
+        self.varianter.data = self.tree
+        self.varianter.parse(argparse.Namespace())
+
+    def test_str_variants(self):
+        self.assertEqual(self.varianter.str_variants(),
+                         ("Variant 1:    /path/path1\n"
+                          "Variant 2:    /path/path2"))
+
+    def test_str_variants_long(self):
+        self.assertEqual(self.varianter.str_variants_long(),
+                         ("Variant 1: /path/path1\n\n"
+                          "    /path/path1:key1 => value1\n\n"
+                          "Variant 2: /path/path2\n\n"
+                          "    /path/path2:key2 => value2\n"))
+
+    def test_str_long(self):
+        self.assertEqual(self.varianter.str_long(),
+                         ("Multiplex tree representation:\n"
+                          " \\-- path\n"
+                          "      #== path1\n"
+                          "      #      -> key1: value1\n"
+                          "      #== path2\n"
+                          "             -> key2: value2\n\n"
+                          "Variant 1:    /path/path1\n"
+                          "Variant 2:    /path/path2\n"))
