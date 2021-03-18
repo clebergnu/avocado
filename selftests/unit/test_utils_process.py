418df69808c26875e7a10a5ef27cd8c76d7fffc2
Cleber Rosa <crosa@redhat.com>
2021-03-18 13:49:47 -0400

    avocado.utils.process: introduce Linux capability utilities
    These utilities are simple wrappers around the libcap command line
    utility, that make it easier to check if a process has a given
    capability.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- selftests/unit/test_utils_process.py
+++ selftests/unit/test_utils_process.py
@@ -671,5 +671,29 @@ class GetCommandOutputPattern(unittest.TestCase):
         self.assertEqual(res, [])
 
 
+class GetCapabilities(unittest.TestCase):
+
+    def test_get_capabilities(self):
+        stderr = b"""Capabilities for `3114520': = cap_chown,cap_dac_override,cap_dac_read_search,cap_fowner,cap_fsetid,cap_kill,cap_setgid,cap_setuid,cap_setpcap,cap_linux_immutable,cap_net_bind_service,cap_net_broadcast,cap_net_admin,cap_net_raw,cap_ipc_lock,cap_ipc_owner,cap_sys_module,cap_sys_rawio,cap_sys_chroot,cap_sys_ptrace,cap_sys_pacct,cap_sys_admin,cap_sys_boot,cap_sys_nice,cap_sys_resource,cap_sys_time,cap_sys_tty_config,cap_mknod,cap_lease,cap_audit_write,cap_audit_control,cap_setfcap,cap_mac_override,cap_mac_admin,cap_syslog,cap_wake_alarm,cap_block_suspend,cap_audit_read,38,39+ep"""
+        cmd_result = process.CmdResult(stderr=stderr, exit_status=0)
+        expected = ['cap_chown', 'cap_dac_override', 'cap_dac_read_search',
+                    'cap_fowner', 'cap_fsetid', 'cap_kill', 'cap_setgid',
+                    'cap_setuid', 'cap_setpcap', 'cap_linux_immutable',
+                    'cap_net_bind_service', 'cap_net_broadcast',
+                    'cap_net_admin', 'cap_net_raw', 'cap_ipc_lock',
+                    'cap_ipc_owner', 'cap_sys_module', 'cap_sys_rawio',
+                    'cap_sys_chroot', 'cap_sys_ptrace', 'cap_sys_pacct',
+                    'cap_sys_admin', 'cap_sys_boot', 'cap_sys_nice',
+                    'cap_sys_resource', 'cap_sys_time', 'cap_sys_tty_config',
+                    'cap_mknod', 'cap_lease', 'cap_audit_write',
+                    'cap_audit_control', 'cap_setfcap', 'cap_mac_override',
+                    'cap_mac_admin', 'cap_syslog', 'cap_wake_alarm',
+                    'cap_block_suspend', 'cap_audit_read', '38', '39+ep']
+        with unittest.mock.patch('avocado.utils.process.run',
+                                 return_value=cmd_result):
+            capabilities = process.get_capabilities()
+        self.assertEqual(capabilities, expected)
+
+
 if __name__ == "__main__":
     unittest.main()
