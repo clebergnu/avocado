86ec621918fea0de73f1064c0221c6fc44f7fbb7
Cleber Rosa <crosa@redhat.com>
2021-05-21 18:19:12 -0400

    Revert "Allow avocado command line app to run from within a wheel"
    This reverts commit 47761a3a08c4e171e4bb967d72bee55fd43e91be.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/__main__.py
+++ avocado/__main__.py
@@ -2,21 +2,9 @@
 Main entry point when called by 'python -m'.
 """
 
-import os
 import sys
 
-
-# If we are running from a wheel, add the wheel to sys.path
-# This allows the usage python $name.whl/module
-if __package__ == "":
-    # __file__ is avocado_framework*.whl/avocado/__main__.py
-    # first dirname call strips of '/__main__.py', second strips off '/avocado'
-    # Resulting path is the name of the wheel itself
-    # Add that to sys.path so we can import pip
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
-
+from avocado.core.main import main
 
 if __name__ == '__main__':
-    from avocado.core.main import main
     sys.exit(main())
