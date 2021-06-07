4c2015f475e9556162482c876c4b33d6702982e6
Cleber Rosa <crosa@redhat.com>
2021-06-06 22:33:56 -0400

    safeloader: introduce module for keeping track of imported symbols
    The "safeloader.module.PythonModule" users a dictionary with
    filesystem path-like value for keeping track of imported modules.
    This is not the most effective format for that information, given that
    it assumes the current module location as the start of the path.  If
    the import is referring to an importable symbol that lives in another
    directory, the filesystem path-like will not follow and be aware of
    that location.
    
    This introduces a proper module that expands the amount of information
    on imported symbols so that a better informed decision can be made
    when trying to find the actual modules files (for further safe
    parsing).
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ avocado/core/safeloader/imported.py
@@ -0,0 +1,107 @@
+import ast
+import os
+
+
+class ImportedSymbol:
+    """A representation of a importable symbol.
+
+    Attributes:
+
+    symbol : str
+    module_path : str or None
+    """
+
+    def __init__(self, symbol, module_path='', importer_fs_path=None):
+        #: The name of the imported symbol.  On a statement such as
+        #: "import os", the symbol is "os".  On a statement such as
+        #: "from unittest.mock import mock_open", the symbol is "mock_open"
+        self.symbol = symbol
+        #: Path from where the symbol was imported.  On a statement such as
+        #: "import os", module_path is None.  On a statement such as
+        #: "from unittest.mock import mock_open", the module_path is
+        #: "unittest.mock".  On a statement such as "from ..foo import bar",
+        #: module_path is "..foo" (relative).
+        self.module_path = module_path
+        #: The full, absolute path of the module importing this symbol.
+        #: This is used for relative path calculations.
+        self.importer_fs_path = importer_fs_path
+
+    @staticmethod
+    def get_symbol_from_statement(statement):
+        if isinstance(statement, ast.ImportFrom):
+            return statement.names[0].name
+
+        if isinstance(statement, ast.Import):
+            if getattr(statement, 'module', None) is not None:
+                # Module has a name, so its path is absolute, and not relative
+                # to the directory structure
+                module_path = statement.module
+            else:
+                relative_level = getattr(statement, 'level', 0)
+                relative_path = "".join(["." for _ in range(relative_level)])
+                name = statement.names[0].name
+                module_path = relative_path + name
+            return module_path
+
+        raise ValueError("Statement is not an import: %s" % statement)
+
+    @staticmethod
+    def get_module_path_from_statement(statement):
+        module_path = getattr(statement, 'module', None) or ''
+        if isinstance(statement, ast.Import):
+            if statement.names[0].asname:
+                module_path = statement.names[0].name
+                if '.' in module_path:
+                    module_path = module_path.rsplit('.', 1)[0]
+        # If this is an aliased import, module_path will be
+        # everything but the last component
+        relative_level = getattr(statement, 'level', 0)
+        relative_level_prefix = "".join(["." for _ in range(relative_level)])
+        return relative_level_prefix + module_path
+
+    @classmethod
+    def from_statement(cls, statement, importer_fs_path=None):
+        return cls(cls.get_symbol_from_statement(statement),
+                   cls.get_module_path_from_statement(statement),
+                   importer_fs_path)
+
+    def to_str(self):
+        """Returns a string representation of the plausible statement used."""
+        if not self.module_path:
+            return "import %s" % self.symbol
+        return "from %s import %s" % (self.module_path, self.symbol)
+
+    def is_relative(self):
+        """Returns whether the imported symbol is on a relative path."""
+        return self.module_path.startswith(".")
+
+    def get_relative_module_fs_path(self):
+        """Returns the module filesystem path, based on its relative path
+
+        This assumes that the module path is indeed related to the location
+        of its importer.  This may not be true if the namespaces match, but
+        are distributed across different filesystem paths.
+        """
+        path = self.importer_fs_path
+        for char in self.module_path:
+            if char != ".":
+                break
+            path = os.path.dirname(path)
+        return path
+
+    def get_parent_fs_path(self):
+        if self.is_relative():
+            return self.get_relative_module_fs_path()
+        parent_path = os.path.dirname(self.importer_fs_path)
+        if self.module_path:
+            return os.path.join(parent_path, self.module_path)
+        return parent_path
+
+    def __repr__(self):
+        return '<ImportedSymbol symbol="%s" module_path="%s">' % (self.symbol,
+                                                                  self.module_path)
+
+    def __eq__(self, other):
+        return ((self.symbol == other.symbol) and
+                (self.module_path == other.module_path) and
+                (self.importer_fs_path == other.importer_fs_path))
---
+++ selftests/unit/test_safeloader_imported.py
@@ -0,0 +1,66 @@
+import ast
+import unittest
+
+from avocado.core.safeloader.imported import ImportedSymbol
+
+
+class SymbolAndModulePath(unittest.TestCase):
+
+    def _check(self, input_symbol, input_module_path, input_statement):
+        statement = ast.parse(input_statement).body[0]
+        symbol = ImportedSymbol.get_symbol_from_statement(statement)
+        self.assertEqual(symbol, input_symbol)
+        module_path = ImportedSymbol.get_module_path_from_statement(statement)
+        self.assertEqual(module_path, input_module_path)
+        imported_symbol = ImportedSymbol(symbol, module_path)
+        self.assertEqual(imported_symbol.to_str(), input_statement)
+        self.assertEqual(imported_symbol,
+                         ImportedSymbol.from_statement(statement))
+
+    def test_symbol_only(self):
+        self._check("os", "", "import os")
+
+    def test_symbol_module_path(self):
+        self._check("path", "os", "from os import path")
+
+    def test_symbol_module_path_compound(self):
+        self._check("mock_open", "unittest.mock",
+                    "from unittest.mock import mock_open")
+
+    def test_symbol_module_path_only_relative(self):
+        self._check("utils", "..", "from .. import utils")
+
+    def test_symbol_module_path_from_relative(self):
+        self._check("utils", "..selftests", "from ..selftests import utils")
+
+    def test_incorrect_statement_type(self):
+        statement = ast.parse("pass").body[0]
+        with self.assertRaises(ValueError):
+            _ = ImportedSymbol.get_symbol_from_statement(statement)
+
+
+class ImporterPath(unittest.TestCase):
+
+    def test_relative_path(self):
+        statement = ast.parse("from ..selftests import utils").body[0]
+        importer = "/abs/fs/location/of/selftests/unit/test_foo.py"
+        symbol = ImportedSymbol.from_statement(statement,
+                                               importer)
+        self.assertEqual(symbol.get_relative_module_fs_path(),
+                         "/abs/fs/location/of/selftests")
+
+    def test_relative_path_same_level(self):
+        statement = ast.parse("from .unit import test_bar").body[0]
+        importer = "/abs/fs/location/of/selftests/unit/test_foo.py"
+        symbol = ImportedSymbol.from_statement(statement,
+                                               importer)
+        self.assertEqual(symbol.get_relative_module_fs_path(),
+                         "/abs/fs/location/of/selftests/unit")
+
+    def test_path_compound(self):
+        statement = ast.parse("from path import parent3").body[0]
+        importer = "/abs/fs/location/of/imports.py"
+        symbol = ImportedSymbol.from_statement(statement,
+                                               importer)
+        self.assertEqual(symbol.get_parent_fs_path(),
+                         "/abs/fs/location/of/path")
