206e54fe5a2113913dbf6217d3199a40a0d616e1
Cleber Rosa <crosa@redhat.com>
2021-10-26 19:31:31 -0400

    Fix auto status servers with long dirs
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- avocado/plugins/runner_nrunner.py
+++ avocado/plugins/runner_nrunner.py
@@ -21,6 +21,7 @@ import multiprocessing
 import os
 import platform
 import random
+import tempfile
 from copy import deepcopy
 
 from avocado.core import nrunner
@@ -277,10 +278,15 @@ class Runner(RunnerInterface):
         return runtime_tasks
 
     def _determine_status_server_uri(self, test_suite, job):
+        # pylint: disable=W0201
+        self.status_server_dir = None
         if test_suite.config.get('nrunner.status_server_auto'):
             # no UNIX domain sockets on Windows
             if platform.system() != 'Windows':
-                return os.path.join(job.logdir, '.status_server.sock')
+                self.status_server_dir = tempfile.TemporaryDirectory(
+                    prefix='avocado_')
+                return os.path.join(self.status_server_dir.name,
+                                    '.status_server.sock')
         return test_suite.config.get('nrunner.status_server_listen')
 
     def _create_status_server(self, test_suite, job):
@@ -368,6 +374,8 @@ class Runner(RunnerInterface):
 
         job.result.end_tests()
         self.status_server.close()
+        if self.status_server_dir is not None:
+            self.status_server_dir.cleanup()
 
         # Update the overall summary with found test statuses, which will
         # determine the Avocado command line exit status
