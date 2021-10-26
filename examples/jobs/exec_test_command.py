33b6ca2960a35c624f28db903547dd8491712a00
Cleber Rosa <crosa@redhat.com>
2021-10-26 19:47:19 -0400

    exec-test: allow a fixed command to be defined
    And with that, allow the use of references as the positional argument
    to the defined command.
    
    This allow users to achieve the same behavior as the "external runner"
    feature of the legacy runner.
    
    With the removal of the legacy runner, we can evaluate if a layer of
    compatibility for the "--external-runner" command line option should
    be added on top of this feature.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

---
+++ examples/jobs/exec_test_command.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+from avocado.core.job import Job
+
+job_config = {'resolver.references': ['https://avocado-project.org'],
+              'resolver.exec_test.command': '/usr/bin/curl'}
+
+with Job.from_config(job_config=job_config) as job:
+    sys.exit(job.run())
---
+++ examples/jobs/exec_test_command_podman.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+from avocado.core.job import Job
+
+job_config = {'resolver.references': ['/etc/fedora-release'],
+              'resolver.exec_test.command': '/bin/cat',
+              'nrunner.spawner': 'podman',
+              'spawner.podman.image': 'fedora:rawhide'}
+
+with Job.from_config(job_config=job_config) as job:
+    sys.exit(job.run())
